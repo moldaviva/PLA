911df42b0c2ef801ffcf08ca9bb1e0df55bfee43
Zaharia Victor <fodor.vic@gmail.com>
2017-12-05 19:21:14 +0200

    CHanging last file

--- Exercitiu_rezolvat_Laborator8.asm
+++ Exercitiu_rezolvat_Laborator8.asm
@@ -2,7 +2,7 @@
 .model flat, stdcall
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;includem biblioteci, si declaram ce functii vrem sa importam
+;@includem biblioteci, si declaram ce functii vrem sa importam
 includelib msvcrt.lib
 extern exit: proc
 extern printf: proc
